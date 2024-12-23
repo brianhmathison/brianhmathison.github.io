5aba297e6b70855355841c32dd391c2dc4890cf3
Brian H Mathison <brianhmathison@gmail.com>
2024-12-23 09:48:06 -0800

    Commit after yarn

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let userName = prompt("Your name?", "Alice");
+let isTeaWanted = confirm("Do you want some tea?");
+
+alert( "Visitor: " + userName ); // Alice
+alert( "Tea wanted: " + isTeaWanted ); // true
