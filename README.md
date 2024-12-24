9ecd7a7113bdea6b25e9d1ce319292e590dd1c4f
Brian H Mathison <brianhmathison@gmail.com>
2024-12-24 15:42:08 -0800

    Edit Script Deleted Learning Files

--- README.md
+++
@@ -1,5 +0,0 @@
-# All Things Tox Future Home
-
-## brianhmathison.github.io
-
-Personal Website Brian H Mathison
--- scripts.js
+++
@@ -1,5 +0,0 @@
-let userName = prompt("Your name?", "Alice");
-let isTeaWanted = confirm("Do you want some tea?");
-
-alert( "Visitor: " + userName ); // Alice
-alert( "Tea wanted: " + isTeaWanted ); // true
