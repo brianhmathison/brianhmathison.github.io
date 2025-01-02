201a9380a1f14338978c2d27c2053cd9034835cf
Brian H Mathison <brianhmathison@gmail.com>
2025-01-02 08:30:02 -0800

    Wonder why this here

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function firstOrNull(array) {
-    return array.length === 0 ? null : array[0];
-}
-firstOrNull(["Rod", "Jane", "Fred"]);
-firstOrNull([1, 2, 3]);
-var first = firstOrNull(["Rod", "Jane", "Fred"]);
-console.log(first);
--- scripts.ts
+++ scripts.ts
@@ -1,3 +1,24 @@
-interface Form<T> {
+// TODO - create the Form generic type
+type Form<T> = {
+  errors: {
+    [K in keyof T]?: string;
+  };
   values: T;
+};
+
+interface Contact {
+  name: string;
+  email: string;
 }
+
+const contactForm: Form<Contact> = {
+  errors: {
+    email: "This must be a valid email address"
+  },
+  values: {
+    name: "Bob",
+    email: "bob@someemail.com"
+  }
+};
+
+console.log(contactForm);
